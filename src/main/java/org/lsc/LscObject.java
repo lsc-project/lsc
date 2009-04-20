054b3f03418bc188ec18bc69a338eb7fa08da123
Jonathan CLARKE <jonathan.clarke@normation.com>
2009-04-20 12:20:59 +0000

    Just indenting and commenting some code.

--- src/main/java/org/lsc/LscObject.java
+++ src/main/java/org/lsc/LscObject.java
@@ -51,39 +51,87 @@ import java.util.List;
 import org.apache.log4j.Logger;
 
 /**
- * @author rschermesser
- *
+ * All objects in LSC inherit from this class, both pure-LDAP style objects
+ * (including top), and flat, database-style objects.
+ * 
+ * @author Rémy-Christophe Schermesser &lt;rcs@lsc-project.org&gt;
  */
-public abstract class LscObject {
-    /** The local logger */
-    protected static Logger LOGGER = Logger.getLogger(LscObject.class);
+public abstract class LscObject
+{
+	/* The local logger */
+	protected static Logger LOGGER = Logger.getLogger(LscObject.class);
 
-    protected String distinguishName;
+	protected String distinguishName;
 
-    protected List<String> objectClass = null;
-    
-    protected LscAttributes attributeValues;
+	protected List<String> objectClass = null;
 
-    public final String getDistinguishName() {
-        return distinguishName;
-    }
+	protected LscAttributes attributeValues;
 
-    public final void setDistinguishName(String dn) {
-        this.distinguishName = dn;
-    }
+	/**
+	 * Returns the full distinguished name of this object.
+	 * 
+	 * @return distinguished name of this object
+	 */
+	public final String getDistinguishName()
+	{
+		return distinguishName;
+	}
 
-    public final List<String> getObjectClass() {
-        return objectClass;
-    }
+	/**
+	 * Set the full distinguished name for this object.
+	 * 
+	 * @param dn
+	 *            This object's distinguished name
+	 */
+	public final void setDistinguishName(String dn)
+	{
+		this.distinguishName = dn;
+	}
 
-    public final void setObjectClass(List<String> objectClass) {
-        this.objectClass = objectClass;
-    }
-    
-    public final void setObjectClass(String objectClass) {
-    	if (this.objectClass == null) {
-    		this.objectClass = new ArrayList<String>();
-    	}
-        this.objectClass.add(objectClass);
-    }
+	/**
+	 * Return the list of values for the objectClass of this object
+	 * 
+	 * @return list of values for the objectClass of this object
+	 */
+	public final List<String> getObjectClass()
+	{
+		return objectClass;
+	}
+
+	/**
+	 * Set the list of values for the objectClass of this object.
+	 * <p>
+	 * Note that this will replace any existing list. To add to the existing
+	 * list instead, see {@link #setObjectClass(String)}.
+	 * </P>
+	 * 
+	 * @param objectClass
+	 *            The objectClass list to set
+	 */
+	public final void setObjectClass(List<String> objectClass)
+	{
+		this.objectClass = objectClass;
+	}
+
+	/**
+	 * Add an objectClass value to this object.
+	 * <p>
+	 * Note that this will add a new value to the existing list, it will not
+	 * replace it. To replace the full list see {@link #setObjectClass(List)}.
+	 * </p>
+	 * <p>
+	 * If no list has been set yet, this method will create one.
+	 * </p>
+	 * 
+	 * @param objectClass
+	 *            The objectClass name to add
+	 */
+	public final void setObjectClass(String objectClass)
+	{
+		if (this.objectClass == null)
+		{
+			this.objectClass = new ArrayList<String>();
+		}
+		this.objectClass.add(objectClass);
+	}
 }

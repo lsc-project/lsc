92ccd4dc421776bbe8d0a952d652fc989fc77d42
Sébastien BAHLOUL <sebastien.bahloul@gmail.com>
2009-12-21 11:28:45 +0000

    Add some comments

--- src/main/java/org/lsc/SynchronizeThreadPoolExecutor.java
+++ src/main/java/org/lsc/SynchronizeThreadPoolExecutor.java
@@ -8,21 +8,34 @@ import java.util.concurrent.TimeUnit;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/**
+ * This object is managing LSC tasks. It is now a wrapper for JDK 
+ * ThreadPoolExecutor but may rely on a different implementation  
+ * @author Sebastien Bahloul <seb@lsc-project.org>
+ *
+ */
 public class SynchronizeThreadPoolExecutor extends ThreadPoolExecutor {
 
-	private static long keepAliveTime = 60;
+	static long keepAliveTime = 60;
 
 	BlockingQueue<Runnable> queue;
 
-	static final Logger LOGGER = LoggerFactory
+	/** Default logger */
+	final Logger LOGGER = LoggerFactory
 			.getLogger(SynchronizeThreadPoolExecutor.class);
 
-	public SynchronizeThreadPoolExecutor(int threads, int maxTasksCount) {
+	protected SynchronizeThreadPoolExecutor(int threads, int maxTasksCount) {
 		super(threads, threads, keepAliveTime, TimeUnit.SECONDS, new ArrayBlockingQueue<Runnable>(maxTasksCount));
 		queue = getQueue(); 
 	}
-	
-	public void runTask(SynchronizeTask task) {
+
+	/**
+	 * Run a task
+	 * In the threadpoolexecutor implementation, store it in the queue and let
+	 * the pool consume it as soon as it can, in a FIFO way without any priority
+	 * @param task the runnable object
+	 */
+	protected void runTask(SynchronizeTask task) {
 		if (LOGGER.isDebugEnabled()) {
 			LOGGER.debug("Task count.." + getTaskCount());
 			LOGGER.debug("Queue Size before assigning the task.."
@@ -40,6 +53,12 @@ public class SynchronizeThreadPoolExecutor extends ThreadPoolExecutor {
 
 	}
 	
+	/**
+	 * This method just rename the thread with the taskname as prefix 
+	 * suffixed by a threads counter
+	 * @param t the thread
+	 * @param r the runnable task
+	 */
 	@Override
 	protected void beforeExecute(Thread t, Runnable r) {
 		super.beforeExecute(t, r);

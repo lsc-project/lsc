2dd5dbeeeb39021f0b4ad93cc4744a4c4e4344ff
Sébastien BAHLOUL <sebastien.bahloul@gmail.com>
2011-09-05 21:07:55 +0000

    Fixing Windows commands

--- bin/lsc-agent.bat
+++ bin/lsc-agent.bat
@@ -21,7 +21,6 @@ SET LOG_DIR=%TEMP%
 SET LOG_FILE=%LOG_DIR%\lsc.log
 
 
-
 call:get_java
 
 call:log "Starting LSC Remote Agent"
@@ -34,13 +33,13 @@ set CLASSPATH=.
 set CLASSPATH=!CLASSPATH!
 
 
-%JAVA_COMMAND% -cp "%CLASSPATH%" org.lsc.jmx.LscAgent %*
+"%JAVA_COMMAND%" -cp "%CLASSPATH%" org.lsc.jmx.LscAgent %*
 
 REM LSC finished running
 call:log "LSC Remote Agent finished running"
 
 if ERRORLEVEL 1 (
-	echo "Please look up the log file %LOG_FILE% to see the error"
+	echo Please look up the log file %LOG_FILE% to see the error
 	exit /B ERRORLEVEL
 
 )
@@ -62,18 +61,9 @@ goto:eof
 
 REM Find the java.exe executable
 :get_java
-	IF DEFINED JAVA_HOME (
-		REM Define Java command from JAVA_HOME
-		SET JAVA_COMMAND=%JAVA_HOME%\bin\java.exe
-	) else (
-		SET JAVA_COMMAND=""
-	)
-
-	if not exist %JAVA_COMMAND% (
-		REM Look for Java in the path
-		set PATHQ=%PATH%
-		goto findJava
-	)
+	IF DEFINED JAVA_HOME ( SET JAVA_COMMAND=%JAVA_HOME%\bin\java.exe) ELSE ( SET JAVA_COMMAND=)
+	IF NOT EXIST "%JAVA_COMMAND%" ( SET PATHQ="%PATH%"
+ GOTO findJava  )
 goto:eof
 
 # Explore the path to find Java
@@ -83,10 +73,12 @@ goto:eof
     for /F "delims=; tokens=1,*" %%i in ("%PATHQ%") do set PATHQ=%%j
     if exist %JAVA_COMMAND% goto:eof
     goto findJava 
+
 :WEND
 REM Nothing seems approprite, warn and exit
 call:fatal "No java executable found on PATH or in JAVA_HOME! Aborting."
 call:fatal "Define JAVA_HOME"
+
 EXIT /B 2
 
 

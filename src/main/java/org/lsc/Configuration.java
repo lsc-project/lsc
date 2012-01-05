37ca0c002497f69a9440e7fd09828f1a6c3f6d34
Sébastien BAHLOUL <sebastien.bahloul@gmail.com>
2012-01-05 23:25:35 +0000

    Removing empty block

--- src/main/java/org/lsc/Configuration.java
+++ src/main/java/org/lsc/Configuration.java
@@ -439,7 +439,6 @@ public class Configuration {
 				// Silently bypass mis-configuration because if setUp(String) is called, this method is run first, probably with bad default settings
 				if(Configuration.class.getClassLoader().getResource("etc") != null) {
 					Configuration.setUp(Configuration.class.getClassLoader().getResource("etc").getPath(), false);
-				} else {
 				}
 			}
 		} catch (LscException le) {

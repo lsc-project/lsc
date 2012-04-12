1d9782394888e427fe8644e8048a2106acad93b1
Clément OUDOT <clem.oudot@gmail.com>
2012-04-12 15:14:28 +0000

    Apply patch from revision r1492 from trunk to 2.0 branch

--- bin/lsc.bat
+++ bin/lsc.bat
@@ -1,5 +1,42 @@
 @echo off
 
+REM ---------------------------------------------------------------------------------
+REM 
+REM Copyright (c) 2008 - 2011 LSC Project 
+REM All rights reserved.
+REM 
+REM Redistribution and use in source and binary forms, with or without modification,
+REM are permitted provided that the following conditions are met:
+REM 
+REM   * Redistributions of source code must retain the above copyright notice, this
+REM     list of conditions and the following disclaimer.
+REM   * Redistributions in binary form must reproduce the above copyright notice,
+REM     this list of conditions and the following disclaimer in the documentation
+REM     and/or other materials provided with the distribution.
+REM   * Neither the name of the LSC Project nor the names of its contributors may be
+REM     used to endorse or promote products derived from this software without
+REM     specific prior written permission.
+REM 
+REM THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+REM ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+REM WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+REM DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+REM ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+REM (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+REM OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+REM THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+REM NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+REM IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+REM 
+REM       (c) 2008 - 2011 LSC Project
+REM   Sebastien Bahloul <seb@lsc-project.org>
+REM   Thomas Chemineau <thomas@lsc-project.org>
+REM   Jonathan Clarke <jon@lsc-project.org>
+REM   Remy-Christophe Schermesser <rcs@lsc-project.org>
+REM 
+REM ---------------------------------------------------------------------------------
+
+
 REM ====================================================================
 REM  work out where LSC lives
 
@@ -20,7 +57,7 @@ call:get_java
 
 call:log "Starting LSC"
 
-set CLASSPATH=.
+set CLASSPATH=%CLASSPATH%;.
  for  %%a in ("%LIB_DIR%\slf4j-api-*.jar") do (
    set CLASSPATH=!CLASSPATH!;%%a
  )
@@ -32,7 +69,7 @@ set CLASSPATH=!CLASSPATH!
 REM if LSC options include the "-a" flag, set the required JMX options
 FOR %%i IN ( %* ) DO IF "%%i"=="-a" (
   IF DEFINED LSC_JMXPORT (
-        SET JAVA_OPTS=-Dcom.sun.management.jmxremote=true -Dcom.sun.management.jmxremote.port=%LSC_JMXPORT% -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false
+	SET JAVA_OPTS=-Dcom.sun.management.jmxremote=true -Dcom.sun.management.jmxremote.port=%LSC_JMXPORT% -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.ssl=false
   ) ELSE (
     call:log "LSC: to control your asynchronous task(s), consider setting the LSC_JMXPORT environment variable to a positive value to bind the JMX interface to that TCP port."
   )
@@ -71,7 +108,7 @@ REM Find the java.exe executable
  GOTO findJava  )
 goto:eof
 
-# Explore the path to find Java
+REM Explore the path to find Java
 :findJava
     if "%PATHQ%"=="" goto WEND
     for /F "delims=;" %%i in ("%PATHQ%") do set JAVA_COMMAND=%%i\java.exe
@@ -80,7 +117,7 @@ goto:eof
     goto findJava 
 
 :WEND
-REM Nothing seems approprite, warn and exit
+REM Nothing seems appropriate, warn and exit
 call:fatal "No java executable found on PATH or in JAVA_HOME! Aborting."
 call:fatal "Define JAVA_HOME"
 

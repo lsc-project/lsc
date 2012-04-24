0a8f81873a71d56e33a9a6f66e0a46c2f0eecc90
Sébastien BAHLOUL <sebastien.bahloul@gmail.com>
2012-04-24 21:32:47 +0000

    Fixing #449: testing invalid configuration will return a non-null value

--- src/main/java/org/lsc/Launcher.java
+++ src/main/java/org/lsc/Launcher.java
@@ -192,8 +192,13 @@ public final class Launcher {
 				}
 				// if a configuration directory was set on command line, use it to set up Configuration
 				Configuration.setUp(configurationLocation, true);
-				LOGGER.info("Configuration and environment successfully checked !");
-				return 0;
+				if(LscConfiguration.isInitialized()) {
+	                LOGGER.info("Configuration and environment successfully checked !");
+	                return 0;
+				} else {
+				    LOGGER.info("Configuration validation failed !");
+				    return 255;
+				}
 			}
 			
 			// if a configuration directory was set on command line, use it to set up Configuration

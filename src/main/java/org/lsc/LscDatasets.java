c0f6271f931593224e793c35ca12d4116b84d35c
Raphaël OUAZANA <raphael.ouazana@gmail.com>
2014-02-21 10:46:23 +0000

    Fix ExecutableLdifDestinationService (references #446)

--- src/main/java/org/lsc/LscDatasets.java
+++ src/main/java/org/lsc/LscDatasets.java
@@ -94,18 +94,18 @@ public class LscDatasets implements Serializable {
 	}
 
 	@SuppressWarnings("unchecked")
-	public List<Object> getListValueAttribute(String attribute) {
-		return (List<Object>) values.get(attribute);
+	public Set<Object> getListValueAttribute(String attribute) {
+		return (Set<Object>) values.get(attribute);
 	}
 
 	@SuppressWarnings("unchecked")
-	public List<String> getListStringValueAttribute(String attribute) {
-		return (List<String>) values.get(attribute);
+	public Set<String> getListStringValueAttribute(String attribute) {
+		return (Set<String>) values.get(attribute);
 	}
 
 	@SuppressWarnings("unchecked")
-	public List<Integer> getListIntegerValueAttribute(String attribute) {
-		return (List<Integer>) values.get(attribute);
+	public Set<Integer> getListIntegerValueAttribute(String attribute) {
+		return (Set<Integer>) values.get(attribute);
 	}
 
 	public List<String> getAttributesNames() {

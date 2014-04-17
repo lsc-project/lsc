e13060fdc5f8299b9b22a09611643fea29f91bf0
Sébastien BAHLOUL <sebastien.bahloul@gmail.com>
2014-04-17 22:36:45 +0000

    Add the message to the exception

--- src/main/java/org/lsc/exception/LscConfigurationException.java
+++ src/main/java/org/lsc/exception/LscConfigurationException.java
@@ -63,6 +63,6 @@ public class LscConfigurationException extends LscException {
 	}
 	
 	public LscConfigurationException(Throwable e) {
-		super("Configuration exception: ", e);
+		super("Configuration exception: " + e.getLocalizedMessage(), e);
 	}
 }

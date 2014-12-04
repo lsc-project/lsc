997b86865d820d2accfe357178f68709a1f19b44
Sébastien BAHLOUL <sebastien.bahloul@gmail.com>
2014-12-04 23:48:38 +0000

    Fixing #743

--- src/main/java/org/lsc/utils/ScriptingEvaluator.java
+++ src/main/java/org/lsc/utils/ScriptingEvaluator.java
@@ -7,11 +7,13 @@ import java.util.Map;
 import javax.script.ScriptEngineFactory;
 import javax.script.ScriptEngineManager;
 
-import org.apache.commons.collections.map.LRUMap;
 import org.lsc.Task;
 import org.lsc.exception.LscServiceException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
 
 public class ScriptingEvaluator {
 
@@ -19,33 +21,36 @@ public class ScriptingEvaluator {
 	
 	/**
 	 * The instances, one per thread to protect non thread safe engines like
-	 * Rhino.!
+	 * Rhino.
 	 */
-	private static LRUMap instances = new LRUMap(15, 0.75f);
+	private static Cache<Object, Object> instancesCache;
 
 	private static Map<String, Class<? extends ScriptableEvaluator>> implementetionsCache;
 
-	private Map<String, ScriptableEvaluator> instancesCache;
+    public static ScriptEngineManager mgr;
+
+    private Map<String, ScriptableEvaluator> instancesTypeCache;
 
 	private ScriptableEvaluator defaultImplementation;
 
 	static {
 		implementetionsCache = new HashMap<String, Class<? extends ScriptableEvaluator>>();
+        instancesCache = CacheBuilder.newBuilder().maximumSize(15).build();
+        mgr = new ScriptEngineManager();
 	}
 
 	private ScriptingEvaluator() {
-		instancesCache = new HashMap<String, ScriptableEvaluator>();
-		ScriptEngineManager mgr = new ScriptEngineManager();
+		instancesTypeCache = new HashMap<String, ScriptableEvaluator>();
 		List<ScriptEngineFactory> factories = mgr.getEngineFactories();
 		for (ScriptEngineFactory sef : factories) {
 			boolean loaded = true;
 			for (String name : sef.getNames()) {
 				if ("js".equals(name)) {
-					instancesCache.put(name,
+					instancesTypeCache.put(name,
 							new JScriptEvaluator(sef.getScriptEngine()));
 					break;
 				} else if ("groovy".equals(name)) {
-					instancesCache.put("gr",
+					instancesTypeCache.put("gr",
 							new GroovyEvaluator(sef.getScriptEngine()));
 					break;
 				}
@@ -56,24 +61,25 @@ public class ScriptingEvaluator {
 			}
 		}
         // Add the rhino engine without debugging capabilities
-		instancesCache.put("rjs",
+		instancesTypeCache.put("rjs",
                 new RhinoJScriptEvaluator(false));
         // Add the rhino engine with debugging capabilities
-        instancesCache.put("rdjs",
+        instancesTypeCache.put("rdjs",
                 new RhinoJScriptEvaluator(true));
 
         // Default to Javascript
-		defaultImplementation = instancesCache.get("js");
+		defaultImplementation = instancesTypeCache.get("js");
 	}
 
 	public static ScriptingEvaluator getInstance() {
 		String threadName = Thread.currentThread().getName();
-		ScriptingEvaluator scriptingEvaluator = (ScriptingEvaluator) instances
-				.get(threadName);
-		if (scriptingEvaluator == null) {
-			scriptingEvaluator = new ScriptingEvaluator();
-			instances.put(threadName, scriptingEvaluator);
-		}
+        ScriptingEvaluator scriptingEvaluator = null;
+
+	    scriptingEvaluator = (ScriptingEvaluator) instancesCache.getIfPresent(threadName);
+        if (scriptingEvaluator == null) {
+            scriptingEvaluator = new ScriptingEvaluator();
+            instancesCache.put(threadName, scriptingEvaluator);
+        }
 		return scriptingEvaluator;
 	}
 
@@ -85,8 +91,8 @@ public class ScriptingEvaluator {
 	private ScriptableEvaluator identifyScriptingEngine(String expression) {
 		String[] parts = expression.split(":");
 		if (parts != null && parts.length > 0 && parts[0].length() < 10
-				&& instancesCache.containsKey(parts[0])) {
-			return instancesCache.get(parts[0]);
+				&& instancesTypeCache.containsKey(parts[0])) {
+			return instancesTypeCache.get(parts[0]);
 		}
 		return defaultImplementation;
 	}
@@ -99,7 +105,7 @@ public class ScriptingEvaluator {
 	private String removePrefix(String expression) {
         String[] parts = expression.split(":");
         if (parts != null && parts.length > 0 && parts[0].length() < 10
-                && instancesCache.containsKey(parts[0])) {
+                && instancesTypeCache.containsKey(parts[0])) {
             return expression.substring(expression.indexOf(":") + 1);
         }
         return expression;

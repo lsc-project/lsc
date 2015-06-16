c810d1b2d5102b4f90b0aef7ce13869bd0f2ee8c
Raphaël OUAZANA <raphael.ouazana@gmail.com>
2015-06-16 07:41:51 +0000

    Allow to set no defaultDelimiter (references #762)

--- src/main/java/org/lsc/configuration/package-info.java
+++ src/main/java/org/lsc/configuration/package-info.java
@@ -2,7 +2,7 @@
 // Ce fichier a été généré par l'implémentation de référence JavaTM Architecture for XML Binding (JAXB), v2.2.6 
 // Voir <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a> 
 // Toute modification apportée à ce fichier sera perdue lors de la recompilation du schéma source. 
-// Généré le : 2015.02.27 à 04:49:36 PM CET 
+// Généré le : 2015.06.16 à 09:37:26 AM CEST 
 //
 
 @javax.xml.bind.annotation.XmlSchema(namespace = "http://lsc-project.org/XSD/lsc-core-2.2.xsd", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)

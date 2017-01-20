2d344c000f68038bff1ff6e278d612e351658575
Soisik Froger <soisik.froger@gmail.com>
2017-01-20 14:22:23 +0100

    Add logs for investigation on broken travis build I cannot reproduce

--- src/test/java/org/lsc/utils/GroovyEvaluatorTest.java
+++ src/test/java/org/lsc/utils/GroovyEvaluatorTest.java
@@ -6,9 +6,6 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
-import mockit.Mocked;
-import mockit.NonStrictExpectations;
-
 import org.codehaus.groovy.jsr223.GroovyScriptEngineFactory;
 import org.junit.Before;
 import org.junit.Test;
@@ -18,10 +15,18 @@ import org.lsc.configuration.TaskType;
 import org.lsc.exception.LscServiceException;
 import org.lsc.jndi.SimpleJndiDstService;
 import org.lsc.jndi.SimpleJndiSrcService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import mockit.Mocked;
+import mockit.NonStrictExpectations;
+
 
 
 public class GroovyEvaluatorTest {
 
+	private static final Logger LOGGER = LoggerFactory.getLogger(GroovyEvaluatorTest.class);
+	
 	private ScriptableEvaluator evaluator;
 	
 	@Mocked Task task;
@@ -34,6 +39,7 @@ public class GroovyEvaluatorTest {
 	@Test
 	public void test1() throws LscServiceException {
 
+
 		new NonStrictExpectations() {
 			{
 				TaskType taskConf = LscConfiguration.getTask("ldap2ldapTestTask");
@@ -45,16 +51,28 @@ public class GroovyEvaluatorTest {
 		Map<String, Object> params = new HashMap<String, Object>();
 		params.put("a", "b");
 		params.put("b", "a");
+		
+		LOGGER.info("Hello b => " + evaluator.evalToString(task,  	"'Hello ' + a", params));
+		
 		assertEquals("Hello b", evaluator.evalToString(task,  	"'Hello ' + a", params));
+		
+		LOGGER.info("[Hello b] => " + evaluator.evalToStringList(task, "'Hello ' + a", params));
+		
 		assertEquals(Arrays.asList(new String[] {"Hello b"}), evaluator.evalToStringList(task, "'Hello ' + a", params));
 		
 		params.put("a", new String[] { "b", "c" } );
+		
+		LOGGER.info("[Hello [b, c]] => " + evaluator.evalToStringList(task, "'Hello ' + a", params));
+		
 		assertEquals(Arrays.asList(new String[] {"Hello [b, c]"}), evaluator.evalToStringList(task, "'Hello ' + a", params));
 		
 		String complexExpression = "def dataToStringEquality = { a, b -> \n" +
-		  	" return a.toString() == b.toString() \n" + 
-		  	"}\n" +
-		  	"dataToStringEquality a, b";
+			" return a.toString() == b.toString() \n" + 
+			"}\n" +
+			"dataToStringEquality a, b";
+		
+		LOGGER.info("false => " + evaluator.evalToBoolean(task, complexExpression, params));
+		
 		assertEquals(false, evaluator.evalToBoolean(task, complexExpression, params));
 	}
 }

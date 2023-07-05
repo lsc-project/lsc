69b4c7c10172f3ccdf7b5092444c89e4428b00e8
David Coutadeur <david.coutadeur@worteks.com>
2023-07-05 10:47:22 +0200

    better fix for enforcing order of user-defined tasks at launch

--- src/main/java/org/lsc/SimpleSynchronize.java
+++ src/main/java/org/lsc/SimpleSynchronize.java
@@ -57,6 +57,7 @@ import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Set;
 import java.util.TreeMap;
+import java.util.stream.Collectors;
 
 import org.apache.commons.lang.ArrayUtils;
 import org.lsc.beans.IBean;
@@ -138,129 +139,52 @@ public class SimpleSynchronize extends AbstractSynchronize {
 	 */
 	public final boolean launch(final List<String> asyncTasks, final List<String> syncTasks,
 					final List<String> cleanTasks) throws Exception {
-		Boolean foundATask = false;
+		boolean foundATask = false;
 		boolean canClose = true;
 		boolean launchResult = true;
 
-		// Get the list of defined tasks from LSC properties
-		// Iterate on each task
-		boolean isASyncTaskAll = asyncTasks.contains(ALL_TASKS_KEYWORD);
-		boolean isSyncTaskAll = syncTasks.contains(ALL_TASKS_KEYWORD);
-		boolean isCleanTaskAll = cleanTasks.contains(ALL_TASKS_KEYWORD);
-		
 		if(getTasksName() == null) {
 			return false;
 		} else if(getTasks().length == 0) {
 			init();
 		}
 		if(!asyncTasks.isEmpty()) {
-            LscServerImpl.startJmx(this);
+			LscServerImpl.startJmx(this);
 		}
-		
-		if (isSyncTaskAll)
-		{
-			// run all tasks defined in configuration file
-			for (Task task: cache.values()) {
-				foundATask = true;
 
-				if (!launchTask(task, Task.Mode.sync)) {
-					launchResult = false;
-				} else {
-					if(task.getSyncHook() != null && task.getSyncHook() != "") {
-						runPostHook(task.getName(), task.getSyncHook(), task.getTaskType());
-					}
+		Collection<Task> asyncTasksToLaunch = userOrderedTask(asyncTasks, asyncTasks.contains(ALL_TASKS_KEYWORD));
+		Collection<Task> syncTasksToLaunch = userOrderedTask(syncTasks, syncTasks.contains(ALL_TASKS_KEYWORD));
+		Collection<Task> cleanTasksToLaunch = userOrderedTask(cleanTasks, cleanTasks.contains(ALL_TASKS_KEYWORD));
+
+		for (Task task: syncTasksToLaunch) {
+			foundATask = true;
+
+			if (!launchTask(task, Task.Mode.sync)) {
+				launchResult = false;
+			} else {
+				if(task.getSyncHook() != null && task.getSyncHook() != "") {
+					runPostHook(task.getName(), task.getSyncHook(), task.getTaskType());
 				}
 			}
 		}
-		else
-		{
-			// Parse each sync task passed in argument, in the order given by user
-			for (String sTask: syncTasks) {
-				// Parse each task in configuration file
-				for (Task task: cache.values()) {
-					// Launch the task if it fits the task in configuration file
-					if (sTask.equals(task.getName())) {
-						foundATask = true;
+		for (Task task: cleanTasksToLaunch) {
+			foundATask = true;
 
-						if (!launchTask(task, Task.Mode.sync)) {
-							launchResult = false;
-						} else {
-							if(task.getSyncHook() != null && task.getSyncHook() != "") {
-								runPostHook(task.getName(), task.getSyncHook(), task.getTaskType());
-							}
-						}
-					}
+			if (!launchTask(task, Task.Mode.clean)) {
+				launchResult = false;
+			} else {
+				if(task.getCleanHook() != null && task.getCleanHook() != "") {
+					runPostHook(task.getName(), task.getCleanHook(), task.getTaskType());
 				}
 			}
 		}
+		for (Task task: asyncTasksToLaunch) {
+			foundATask = true;
 
-		if (isCleanTaskAll)
-		{
-			// run all tasks defined in configuration file
-			for (Task task: cache.values()) {
-				foundATask = true;
+			canClose = false;
 
-				if (!launchTask(task, Task.Mode.clean)) {
-					launchResult = false;
-				} else {
-					if(task.getCleanHook() != null && task.getCleanHook() != "") {
-						runPostHook(task.getName(), task.getCleanHook(), task.getTaskType());
-					}
-				}
-			}
-		}
-		else
-		{
-			// Parse each clean task passed in argument, in the order given by user
-			for (String cTask: cleanTasks) {
-				// Parse each task in configuration file
-				for (Task task: cache.values()) {
-					// Launch the task if it fits the task in configuration file
-					if (cTask.equals(task.getName())) {
-						foundATask = true;
-
-						if (!launchTask(task, Task.Mode.clean)) {
-							launchResult = false;
-						} else {
-							if(task.getCleanHook() != null && task.getCleanHook() != "") {
-								runPostHook(task.getName(), task.getCleanHook(), task.getTaskType());
-							}
-						}
-					}
-				}
-			}
-		}
-
-		if (isASyncTaskAll)
-		{
-			// run all tasks defined in configuration file
-			for (Task task: cache.values()) {
-				foundATask = true;
-
-				canClose = false;
-
-				if(!launchTask(task, Task.Mode.async)) {
-					launchResult = false;
-				}
-			}
-		}
-		else
-		{
-			// Parse each async task passed in argument, in the order given by user
-			for (String aTask: asyncTasks) {
-				// Parse each task in configuration file
-				for (Task task: cache.values()) {
-					// Launch the task if it fits the task in configuration file
-					if (aTask.equals(task.getName())) {
-						foundATask = true;
-
-						canClose = false;
-
-						if(!launchTask(task, Task.Mode.async)) {
-							launchResult = false;
-						}
-					}
-				}
+			if(!launchTask(task, Task.Mode.async)) {
+				launchResult = false;
 			}
 		}
 
@@ -274,6 +198,18 @@ public class SimpleSynchronize extends AbstractSynchronize {
 		}
 
 		return launchResult;
+	}
+
+	private Collection<Task> userOrderedTask(List<String> userValues, boolean all) {
+		Collection<Task> allTasks = cache.values();
+		if (all) {
+			return allTasks;
+		}
+		Collection<String> allTaskNames = allTasks.stream().map(Task::getName).collect(Collectors.toList());
+		return userValues.stream()
+				.filter(allTaskNames::contains)
+				.map(cache::get)
+				.collect(Collectors.toList());
 	}
 
 	/**

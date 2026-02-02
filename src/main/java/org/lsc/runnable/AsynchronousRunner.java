1c280bd095be66fd7435fd30a351446b61f1ab92
Emmanuel Lécharny <emmlec@worteks.com>
2026-02-02 15:58:35 +0100

    o Handle entries deletions when using a LDAP source and destination o Replacement of the asynchronous polling system by an event-loop like system o Addition of a message for when the 'refresh' phase is completed before switching to the 'persist' phase o Replacement of declaration like 'new String[getAttrsId().size()]' by 'new String[0]', because it's more efficient and readable Use of a static pattern for filters o Use of iterator on Cursor for code clarity

--- src/main/java/org/lsc/runnable/AsynchronousRunner.java
+++ src/main/java/org/lsc/runnable/AsynchronousRunner.java
@@ -1,11 +1,13 @@
 package org.lsc.runnable;
 
 import java.util.Map.Entry;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.Callable;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+import java.util.function.Supplier;
 
 import org.lsc.AbstractSynchronize;
 import org.lsc.LscDatasets;
-import org.lsc.SynchronizeThreadPoolExecutor;
 import org.lsc.Task;
 import org.lsc.beans.InfoCounter;
 import org.lsc.exception.LscServiceException;
@@ -27,56 +29,101 @@ public class AsynchronousRunner implements Runnable {
 	}
 
 	public void run() {
+        int nThreads = abstractSynchronize.getThreads();
 		counter = new InfoCounter();
 
-		SynchronizeThreadPoolExecutor threadPool = new SynchronizeThreadPoolExecutor(abstractSynchronize.getThreads());
-
-		Entry<String, LscDatasets> nextId = null;
-		try {
-			IAsynchronousService aService = null;
-			boolean fromSource = true;
-			if (task.getDestinationService() instanceof IAsynchronousService) {
-				aService = (IAsynchronousService) task.getDestinationService();
-				fromSource = false;
-			} else if (task.getSourceService() instanceof IAsynchronousService) {
-				aService = (IAsynchronousService) task.getSourceService();
-			} else {
-				LOGGER.error("LSC should never reach this point! Please consider debugging the code because we are trying to launch an asynchronous sync without any asynchronous service!"); 
-				return;
-			}
-
-			LOGGER.debug("Asynchronous synchronize {}", task.getName());
-
-			boolean interrupted = false;
-			while (!interrupted) {
-				nextId = aService.getNextId();
-				if (nextId != null) {
-					threadPool.runTask(new SynchronizeEntryRunner(task, counter, abstractSynchronize, nextId, fromSource));
-				} else {
-					try {
-						Thread.sleep(aService.getInterval());
-					} catch (InterruptedException e) {
-						LOGGER.debug("Synchronization thread interrupted !");
-						interrupted = true;
-					}
-				}
-			}
-		} catch (LscServiceException e) {
-			counter.incrementCountError();
-			abstractSynchronize.logActionError(null, nextId, e);
+		final IAsynchronousService asyncService;
+		final boolean fromSource;
+		
+		if (task.getDestinationService() instanceof IAsynchronousService) {
+		    asyncService = (IAsynchronousService) task.getDestinationService();
+			fromSource = false;
+		} else if (task.getSourceService() instanceof IAsynchronousService) {
+		    asyncService = (IAsynchronousService) task.getSourceService();
+		    fromSource = true;
+		} else {
+			LOGGER.error("LSC should never reach this point! Please consider debugging the code because "
+			        + "we are trying to launch an asynchronous sync without any asynchronous service!"); 
+			return;
 		}
 
-		try {
-			threadPool.shutdown();
-			threadPool.awaitTermination(abstractSynchronize.getTimeLimit(), TimeUnit.SECONDS);
-		} catch (InterruptedException e) {
-			LOGGER.error("Tasks terminated according to time limit: " + e.toString(), e);
-			LOGGER.info("If you want to avoid this message, " + "increase the time limit by using dedicated parameter.");
-		}
+		LOGGER.debug("Asynchronous synchronize {}", task.getName());
+        LOGGER.debug("Running tasks with {} threads", nThreads);
+        
+        // We need to protect the getNextid() function against concurrent access
+        Lock lock = new ReentrantLock();
 
-	}
+        // Supplier of tasks, will be called by the thread pool,
+        // returns null if no more tasks to run
+        Supplier<Callable<Object>> supplier = () -> {
+            try {
+                Entry<String, LscDatasets> nextId;
+
+                // Fetch an entry from the source
+                lock.lock();
+                
+                try {
+                    // This is a blocking call, we may wait until we get
+                    // a response
+                    nextId = asyncService.getNextId();
+                } finally {
+                    lock.unlock();
+                }
+                
+                if (nextId == null) {
+                    // we're done
+                    return null;
+                }
+                
+                // Now compute the modifications
+                return new Callable<Object>() {
+                    @Override
+                    public String call() throws Exception {
+                        // Process the entry
+                        SynchronizeEntryRunner entryRunner = new SynchronizeEntryRunner(
+                                task, counter, abstractSynchronize, nextId, fromSource);
+                        
+                        entryRunner.run();
+                        
+                        return nextId.getKey() + " processed";
+                    }
+                };
+            } catch (LscServiceException e) {
+                counter.incrementCountError();
+                abstractSynchronize.logActionError(null, null, e);
+            }
+            
+            return null;
+        };
+
+        // Thread pool, each thread loops until the task supplier returns a null Callable
+        // Basically, fetch an entry from the source, and process the associated callable
+        // It's a fixed sized thread pool, once created the threads are up forever
+        for (int i = 0; i < nThreads; i++) {
+            int id = i;
+            
+            new Thread(() -> {
+                while (true) {
+                    try {
+                        Callable<Object> task = supplier.get();
+                        
+                        if (task == null) {
+                            LOGGER.info("--- Thread {}: done", id);
+                            break;
+                        }
+                        
+                        Object result = task.call();
+                        LOGGER.debug("Thread {} ran task {}", id, result);
+                    } catch (Exception whatShallWeDo) {
+                        whatShallWeDo.printStackTrace();
+                    }
+                }
+            }).start();
+        }
+    }
 
 	public InfoCounter getCounter() {
 		return counter;
 	}
 }
+
